9aecba55322179c5d1e0617c0af473b1058a5886
Zehao Zhu <379196800@qq.com>
2023-12-01 14:13:01 -0800

    B4 finished

---
+++ B4/CancerAnalysisApp.R
@@ -0,0 +1,106 @@
+library(shiny)
+library(datateachr)
+library(ggplot2)
+library(dplyr)
+library(DT)
+
+## This app analyzes the cancer data to see how the diagnosis is related to the worst level/value.
+## It has many functions:
+## 1. It illustrates three outputs (containing one interactive table):
+##      a box and jitter plot of worst values by diagnosis,
+##      a summarize table grouped by diagnosis and worst levels, 
+##      and a full data table
+## 2. A slider input to select the number of worst levels in data. By this function, we can see different plots and data.
+## 3. A check box input to show the legend in the plot. Sometimes, we want to turn on the legend to see the worst level distribution and turn off to see the box clearly.
+## 4. A cancer image to make our dashboard more interesting and to align with the data frame "cancer_sample".
+## 5. Separating tabs. Because I have two tables and one plot, separating tabs to show them makes the dashboard clear.
+## 6. A download button for downloading the result table.
+ui <- fluidPage(
+  titlePanel("Worst Level Distribution by Diagnosis Data Analysis Dashboards"),
+  sidebarLayout(
+    sidebarPanel(
+      # the slider input of the number of worst levels
+      sliderInput("num", "Select how many intervals to cut the worst radius and worst concavity",
+                  min = 3,
+                  max = 10,
+                  value = 4),
+      # allow the user to turn on/off the legend
+      checkboxInput("show_legend", "Check to show legend", value = TRUE),
+      # download button
+      downloadButton("download", label = "Download"),
+      # add a cancer image to UI
+      img(src = "cancer-icon-2797418_1280.png", align = "center", height = 100, width = 100)
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("myplot")), # the output plot of box plot
+        tabPanel("Summarise", tableOutput("mytable_summarise")), # the output summarise table
+        tabPanel("Table", dataTableOutput("mytable")) #the full result table output
+      )
+    )
+  )
+)
+server <- function(input, output){
+  # reactive data
+  num_input <- reactive({input$num}) 
+  show_leg <- reactive({input$show_legend})
+  
+  #Output Worst Value Table:
+  #Cut worst radius and worst concavity into intervals and add/concatenate them together.
+  #After combining the two intervals, we can see the distribution by diagnosis
+  cancer_worst_radius_concavity <- reactive({cancer_sample %>%
+    mutate(radius_worst_level = cut_interval(radius_worst, num_input(), labels = 1:num_input()), 
+           concavity_worst_level = cut_interval(concavity_worst, num_input(), labels = 1:num_input())) %>% # cut the worst radius and concavity into intervals
+    mutate(worst_level = paste0(as.character(radius_worst_level)," ", as.character(concavity_worst_level)), 
+           worst_value = scale(radius_worst)+scale(concavity_worst)) %>%
+    select(diagnosis, worst_level, worst_value, radius_worst, concavity_worst)})
+  
+  # plot output
+  output$myplot <- renderPlot({
+    cur_table <- cancer_worst_radius_concavity()
+    if (show_leg()){
+      cur_table %>%
+        ggplot(aes(x = diagnosis, y = worst_value)) +
+        geom_boxplot()+ # the box plot of worst value by diagnosis
+        geom_jitter(aes(color = worst_level), alpha = 0.3)+ # add jitter to show the distribution of worst level
+        ggtitle("The boxplot of worst value by diagnosis")+
+        xlab("diagnosis")+
+        ylab("worst value")
+    }
+    else{
+      cur_table %>%
+        ggplot(aes(x = diagnosis, y = worst_value)) +
+        geom_boxplot()+ # the box plot of worst value by diagnosis
+        geom_jitter(alpha = 0.3)+ # add jitter to show the distribution of worst level
+        ggtitle("The boxplot of worst value by diagnosis")+
+        xlab("diagnosis")+
+        ylab("worst value")
+    }
+  })
+  
+  # summarise output
+  output$mytable_summarise <- renderTable({
+    cur_table <- cancer_worst_radius_concavity()
+    cur_table %>%
+      group_by(diagnosis, worst_level) %>%
+      summarise(counts = n()) %>% # calculate the counts of each worst concavity level
+      mutate(proportion = counts / sum(counts)) # calculate the proportion of each worst concavity level
+  })
+  
+  # full table output
+  output$mytable <- renderDataTable({ ## out put the entire table in tab three
+    cancer_worst_radius_concavity()
+  })
+  
+  # download output
+  output$download <- downloadHandler(
+    filename = function(){
+      paste("table_", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file){
+      write.csv(cancer_worst_radius_concavity(), file)
+    }
+  )
+}
+shinyApp(ui = ui, server = server)
+
--- README.md
+++ README.md
@@ -1,24 +1,35 @@
-# Penguin Analysis Dashboard
+# Worst Level Distribution by Diagnosis Data Analysis Dashboards (B4)
+
 
 ## App link:
 The link to the running Shiny app of my dashboard is:
-https://zzh2015.shinyapps.io/assignment-b3-BearAlenko/
+https://zzh2015.shinyapps.io/B4CancerAnalysis/
 
 ## Assignment option and dataset:
-* My choice is Option B - my own Shiny app of penguins.
-* Dataset: palmerpenguins from [palmerpenguins package](https://allisonhorst.github.io/palmerpenguins/).
+* My choice is Option C with a new app - my own Shiny app of cancer worst level.
+* Dataset: cancer sample from [datateachr package](https://github.com/UBC-MDS/datateachr).
+
+## File location:
+The code and the image are in /B4.
+* CancerAnalysisApp.R: the main code of the shiny app.
+* www/cancer-icon-2797418_1280.png: the cancer image used in app.
 
 ## Description:
-
-_This app analyzes the palmerpenguins data._
+_This app analyzes the cancer sample data by analyzing the worst levels._
 It has many functions:
 1. It illustrates three outputs:
-     * a **box plot** of flipper length by species.
-     * a **summarize table** grouped by species and islands. 
-     * an **interactive data table**.
-2. A **slider input** to select the max bill length in data. By this function, we can see different plots and data in different subsets of bill length to **gain insights in analysis**.
-3. A **check box** input to sort the table by islands. Sometimes, we want to compare the data on the same island and the sorted table is more **clear to compare** on this.
-4. A **penguin image** to make our dashboard more **interesting and to align with the topic**.
+     * a **box and jitter plot** of worst values by diagnosis.
+     * a **summarize table** grouped by diagnosis and worst levels. 
+     * an **interactive data table** of the full result data.
+2. A **slider input** to select the number of worst levels in data. By this function, we can see different plots and data.
+3. A **check box input** to show the legend in the plot. Sometimes, we want to turn on the legend to see the worst level distribution and turn off to see the box clearly.
+4. A **cancer image** to make our dashboard more interesting and to align with the data frame "cancer_sample".
 5. **Separating tabs**. Because I have two tables and one plot, separating tabs to show them **makes the dashboard clear and pretty**.
 6. **Interactive table** can help users to choose how many data entries to show in a page and sort on different variables.
+7. A **download button** for downloading the result table.
+
+## Methods
+1. I first cut the worst radius and the worst concavity into four
+categories so that we can combine them together as one dimension **“worst level”** for easy diagnosis. The resulted worst_level has dxd categories where d is the chosen number of levels by users. By combining the two level variables, we can simulate an easy model for pre-diagnosis, for example, self diagnosis before seeing a doctor. 
+2. I also calculate **"worst value"**, the sum of normalized worst concavity and worst radius, as a representation of numeric worst level for plotting.
 
